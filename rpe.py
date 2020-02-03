5b7846cfa52039a24dded163949c5a5003c733dd
CHEN， Haohuang <chenhh@dell-mgt-01.ivic.org.cn>
2020-02-03 22:10:51 +0800

    m

--- rpe.py
+++ rpe.py
@@ -18,7 +18,7 @@ use_gpu = torch.cuda.is_available()
 img_h, img_w = 224, 224
 batch_size = 16
 lr_init = 1e-5
-max_epoch = 3
+max_epoch = 8
 
 all_data_list = os.listdir('./camera_train_data')
 test_data_list = random.sample(all_data_list, 500)
@@ -147,10 +147,10 @@ norm_trans = transforms.Compose([
 			])
 
 train_data = MyDataset(train_data_list, './camera_train_data/', norm_trans)
-train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=16)
+train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=64)
 
 test_data = MyDataset(test_data_list, './camera_train_data/', norm_trans)
-test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True, num_workers=8)
+test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True, num_workers=64)
 
 net = RobotJointModel()
 if(use_gpu):
@@ -162,7 +162,7 @@ if(use_gpu):
 	criterion = criterion.cuda()
 #optimizer = torch.optim.SGD(net.parameters(), lr=1e-5, momentum=0.9, dampening=0.1)    # 选择优化器
 optimizer = torch.optim.Adam(net.parameters(), lr=lr_init)    # 选择优化器
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.3)     # 设置学习率下降策略
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)     # 设置学习率下降策略
 # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', verbose=True, threshold=1e-7, min_lr=1e-7, factor=0.9)     # 设置学习率下降策略
 
 for epoch in range(max_epoch):
