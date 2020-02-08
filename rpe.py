1a64d95dd1f97000749b3cafb2e7c545539a8290
CHEN， Haohuang <chenhh@dell-mgt-01.ivic.org.cn>
2020-02-08 16:56:14 +0800

    m

--- rpe.py
+++ rpe.py
@@ -18,10 +18,10 @@ torch.set_num_threads(80)
 img_h, img_w = 224, 224
 batch_size = 16
 lr_init = 1e-5
-max_epoch = 5
+max_epoch = 20
 
 all_data_list = os.listdir('./camera_train_data')
-test_data_list = random.sample(all_data_list, 500)
+test_data_list = random.sample(all_data_list, 2000)
 train_data_list = list(set(all_data_list) - set(test_data_list))
 random.shuffle(test_data_list)
 random.shuffle(train_data_list)
@@ -150,7 +150,7 @@ train_data = MyDataset(train_data_list, './camera_train_data/', norm_trans)
 train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=64)
 
 test_data = MyDataset(test_data_list, './camera_train_data/', norm_trans)
-test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=32)
+test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=64)
 
 net = RobotJointModel()
 if(use_gpu):
@@ -162,7 +162,7 @@ if(use_gpu):
 	criterion = criterion.cuda()
 #optimizer = torch.optim.SGD(net.parameters(), lr=1e-5, momentum=0.9, dampening=0.1)    # 选择优化器
 optimizer = torch.optim.Adam(net.parameters(), lr=lr_init)    # 选择优化器
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)     # 设置学习率下降策略
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)     # 设置学习率下降策略
 # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', verbose=True, threshold=1e-7, min_lr=1e-7, factor=0.9)     # 设置学习率下降策略
 
 for epoch in range(max_epoch):
@@ -195,8 +195,8 @@ for epoch in range(max_epoch):
 			print("Training: Epoch[{:0>3}/{:0>3}] Iteration[{:0>3}/{:0>3}] Loss: {:.8f}".format(
 				epoch + 1, max_epoch, i + 1, len(train_loader), loss_avg))
 		# scheduler.step(loss)  # 更新学习率
-	#if epoch % 2 == 1:
-	#	scheduler.step()
+	if epoch % 2 == 1:
+		scheduler.step()
 
 	loss_sigma = 0.0
 	net.eval()
