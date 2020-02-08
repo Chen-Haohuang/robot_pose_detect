3aaea6a6db895331cf9a01cd3755eac5a92b0e64
chh <chhhappy@buaa.edu.cn>
2020-02-08 10:20:37 +0800

    m

--- train_data_generate.py
+++ train_data_generate.py
@@ -6,7 +6,7 @@ np.set_printoptions(threshold=100000,linewidth=100000)
 img_h,img_w = 224,224
 
 for camera_index in range(1,4):
-    for data_index in range(11000):
+    for data_index in range(12000):
         print(camera_index,"-",data_index)
         depth_img = cv2.imread("./camera_data/camera"+str(camera_index)+"-depth-"+str(data_index)+".png",cv2.IMREAD_GRAYSCALE)
         rgb_img = cv2.imread("./camera_data/camera"+str(camera_index)+"-rgb-"+str(data_index)+".png")
