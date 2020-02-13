7c5d7d72b0d8700ca37d024448d2b58bd3cb7bb5
chh <chhhappy@buaa.edu.cn>
2020-02-13 12:18:51 +0800

    m

--- rpe_angle.py
+++ rpe_angle.py
@@ -1,81 +1,117 @@
-import os
-import torch
-import torchvision
-import torch.nn as nn
-import torchvision.transforms as transforms
-import cv2
 import numpy as np
-from PIL import Image
-from torch.utils.data import Dataset, DataLoader
-import random
-import matplotlib.pyplot as plt
-import target_data_generate
-import re
-import rpe_joint
+import math
+
+joint1_pos_init = np.array([0.000000,0.000000,0.200000])
+joint2_pos_init = np.array([0.030000,0.000000,0.380000])
+joint3_pos_init = np.array([0.030000,0.000000,0.720000])
+joint4_pos_init = np.array([0.150000,0.000000,0.755000])
 
 
-class RobotAngleModel(nn.Module):
 
-    def __init__(self):
-        super(RobotAngleModel, self).__init__()
-        self.stages = nn.Sequential(
-                            nn.Linear(15, 512),
-                            nn.ReLU(),
-                            nn.Linear(512, 128),
-                            nn.ReLU(),
-                            nn.Linear(128, 6)
-        )
+def angle1(joint2_pos):
+    center = np.array([0.000000,0.000000,0.380000])
+    rotate_axis = np.array([0.000000,0.000000,1.000000])
+
+    vector1 = joint2_pos_init - center
+    vector2 = joint2_pos - center
+
+    direction = np.dot(rotate_axis, np.cross(vector1,vector2))
+    # 0 is impossible
+    if(direction < 0.0):
+        direction = -1.0
+    else:
+        direction = 1.0
+
+    cos_angle = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
+
+    angle = direction * math.acos(cos_angle)
+
+    return angle
 
 
-    def forward(self, inputs):
-        output = self.stages(inputs)
-        
-        return output
+def angle2(angle1, joint2_pos, joint3_pos):
+    joint3_angle1_pos = joint2_pos + (joint3_pos_init - joint2_pos_init)
 
-    def _initialize_weights(self, m):
-        if isinstance(m, nn.Conv2d):
-            torch.nn.init.xavier_normal_(m.weight.data)
-            if m.bias is not None:
-                m.bias.data.zero_()
-        elif isinstance(m, nn.BatchNorm2d):
-            m.weight.data.fill_(1)
-            m.bias.data.zero_()
-        elif isinstance(m, nn.Linear):
-            torch.nn.init.normal_(m.weight.data, 0, 0.01)
-            m.bias.data.zero_()
+    center = joint2_pos
+    rotate_axis = np.array([-math.sin(angle1), math.cos(angle1), 0.000000])
 
-    def initialize_weights(self):
-        for m in self.stages:
-            self._initialize_weights(m)
+    vector1 = joint3_angle1_pos - center
+    vector2 = joint3_pos - center
 
-class MyDataset(Dataset):
-    def __init__(self, data_list):
-        imgs_index = data_list
-        self.imgs_index = imgs_index        # 最主要就是要生成这个list， 然后DataLoader中给index，通过getitem读取图片数据
-        self.link_state_target = target_data_generate.gen_link_state_target()
-        self.joint_state_target = target_data_generate.gen_joint_state_target()
+    direction = np.dot(rotate_axis, np.cross(vector1, vector2))
+    # 0 is impossible
+    if(direction < 0.0):
+        direction = -1.0
+    else:
+        direction = 1.0
 
-    def __getitem__(self, index):
-        data_index = self.imgs_index[index]
+    cos_angle = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
 
-        data = [0 for i in range(15)]
+    angle = direction * math.acos(cos_angle)
 
-        u_v,_ = target_data_generate.gen_one_heatmap_target(self.link_state_target, data_index, 2)
-        for j in range(5):
-            data[2+j*3] = u_v[j][0]
-        
-        u_v,_ = target_data_generate.gen_one_heatmap_target(self.link_state_target, data_index, 2)
-        for j in range(5):
-            data[j*3] = u_v[j][0]
-            data[1+j*3] = u_v[j][1]
+    return angle
 
-        data = np.array(data, dtype=np.float32)
+def angle3(angle1, angle2, joint2_pos, joint3_pos, joint4_pos):
+    joint3_angle1_pos = joint2_pos + (joint3_pos_init - joint2_pos_init)
+    _joint3_angle1_pos = np.array([joint3_angle1_pos[0], joint3_angle1_pos[1], 0.755000])
+    _joint3_pos = joint3_pos * (0.720000 - 0.380000) / (0.755000 - 0.380000)
 
-        label = self.joint_state_target[data_index]
-        label = np.array(label, dtype=np.float32)
-        label = np.reshape(label, (6,))
+    joint4_angle1_pos = np.array([0.150000*math.cos(angle1), 0.150000*math.sin(angle1), 0.755000])
 
-        return data , label, data_index
+    joint4_angle2_pos = (np.array([joint4_angle1_pos[0] * math.cos(angle2), 
+                                  joint4_angle1_pos[1] * math.cos(angle2), 
+                                  joint4_angle1_pos[2] - (0.150000*math.sin(angle2))
+                                  ])
+                         - _joint3_angle1_pos) + _joint3_pos
 
-    def __len__(self):
-        return len(self.imgs_index)
+    print(joint4_angle2_pos)
+
+    center = joint3_pos
+    rotate_axis = np.array([-math.sin(angle1), math.cos(angle1), 0.000000])
+
+    vector1 = joint4_angle2_pos - center
+    vector2 = joint4_pos - center
+
+    direction = np.dot(rotate_axis, np.cross(vector1, vector2))
+    # 0 is impossible
+    if(direction < 0.0):
+        direction = -1.0
+    else:
+        direction = 1.0
+
+    cos_angle = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
+
+    angle = direction * math.acos(cos_angle)
+
+    return angle
+
+
+joint2_pos = np.array([0.029874,-0.002752,0.380000])
+joint3_pos = np.array([-0.305885,0.028175,0.423698])
+joint4_pos = np.array([-0.201347,0.018546,0.491550])
+a1 = angle1(joint2_pos)
+print(a1)
+a2 = angle2(a1, joint2_pos, joint3_pos)
+print(a2)
+a3 = angle3(a1, a2, joint2_pos, joint3_pos, joint4_pos)
+print(a3)
+
+print("\n")
+
+joint2_pos = np.array([-0.015225,0.025849,0.380000])
+joint3_pos = np.array([-0.044914,0.076254,0.714930])
+joint4_pos = np.array([-0.076971,0.130680,0.607064])
+a1 = angle1(joint2_pos)
+print(a1)
+a2 = angle2(a1, joint2_pos, joint3_pos)
+print(a2)
+a3 = angle3(a1, a2, joint2_pos, joint3_pos, joint4_pos)
+print(a3)
+
+
+# 0 | 0.000000,0.000000,0.200000 | 0.030000,0.000000,0.380000 | 0.030000,0.000000,0.720000 | 0.150000,0.000000,0.755000 | 0.375000,0.000000,0.755000 | 0.465000,0.000000,0.755000
+# 9 | 0.000000,0.000000,0.200000 | 0.029874,-0.002752,0.380000 | -0.305885,0.028175,0.423698 | -0.201347,0.018546,0.491550 | 0.013349,-0.001230,0.555890 | 0.033281,-0.088622,0.563964
+# 10 | -0.000000,-0.000000,0.200000 | -0.015225,0.025849,0.380000 | -0.044914,0.076254,0.714930 | -0.076971,0.130680,0.607064 | -0.104776,0.177886,0.388836 | -0.146747,0.125017,0.448361
+
+# 9 | 0.885814 | -0.091852 | -1.441917 | 2.063365 | -1.568093 | 1.242607
+# 10 | 1.643832 | 2.103091 | 0.172913 | 2.810962 | 1.428433 | 3.119301
