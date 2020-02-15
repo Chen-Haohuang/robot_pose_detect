2a9215eec2c4ccb50b07cd735bc8a8f2fce50486
chh <chhhappy@buaa.edu.cn>
2020-02-15 20:44:49 +0800

    m

--- rpe.py
+++ rpe.py
@@ -93,7 +93,7 @@ class RobotJointModel(nn.Module):
 		heatmaps = dsntnn.flat_softmax(stage_output)
 		coords = dsntnn.dsnt(heatmaps)
 
-		return coords, heatmaps
+		return coords, stage_output
 
 	def _initialize_weights(self, m):
 		if isinstance(m, nn.Conv2d):
