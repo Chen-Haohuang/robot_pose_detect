9feb30289d74475adbb51e89f048ac3948193dcc
chh <chhhappy@buaa.edu.cn>
2020-02-16 11:30:28 +0800

    m

--- rpe.py
+++ rpe.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 import target_data_generate
 import re
 import dsntnn
-# torch.set_printoptions(threshold=100000000000, linewidth=10000000000)
 
 use_gpu = torch.cuda.is_available()
 torch.set_num_threads(80)
@@ -94,7 +93,7 @@ class RobotJointModel(nn.Module):
 		heatmaps = dsntnn.flat_softmax(stage_output)
 		coords = dsntnn.dsnt(heatmaps)
 
-		return coords, heatmaps
+		return coords, stage_output
 
 	def _initialize_weights(self, m):
 		if isinstance(m, nn.Conv2d):
@@ -123,9 +122,8 @@ class MyDataset(Dataset):
 	def __getitem__(self, index):
 		id = self.imgs_index[index]
 		fn = self.path + id
-
-		img = torch.from_numpy(cv2.imread(fn)).permute(2,0,1).float()
-		img = img.div(255)
+		img = Image.open(fn).convert('RGB')     # 像素值 0~255，在transfrom.totensor会除以255，使像素值变成 0~1
+		img = self.transform(img)   # 在这里做transform，转为tensor等等
 
 		matchObj = re.match(r'camera(.*?)-(.*?).png', id, re.M|re.I)
 		camera_index = int(matchObj.group(1))
@@ -166,7 +164,6 @@ scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)  
 for epoch in range(max_epoch):
 	euc_loss_sigma = 0.0
 	reg_loss_sigma = 0.0    # 记录一个epoch的loss之和
-	loss_sigma = 0.0
 	correct = 0.0
 	total = 0.0
 	pre_i = -1
@@ -183,9 +180,9 @@ for epoch in range(max_epoch):
 
 		coords, heatmaps = net(inputs)
 
-		euc_losses = dsntnn.euclidean_losses(coords, coord_labels)
-		reg_losses = dsntnn.js_reg_losses(heatmaps, coord_labels, sigma_t=1.0)
-		loss = dsntnn.average_loss(euc_losses + reg_losses)
+		euc_losses = criterion(coords, coord_labels.float())
+		reg_losses = criterion(heatmaps, heatmaps_labels.float())
+		loss = euc_losses + reg_losses
 
 		optimizer.zero_grad()
 		loss.backward()
@@ -194,27 +191,22 @@ for epoch in range(max_epoch):
 		if(use_gpu):
 			euc_loss = euc_losses.cpu()
 			reg_loss = reg_losses.cpu()
-			loss = loss.cpu()
-		euc_loss_sigma += dsntnn.average_loss(euc_losses).item()
-		reg_loss_sigma += dsntnn.average_loss(reg_losses).item()
-		loss_sigma += loss.item()
+		euc_loss_sigma += euc_losses.item()
+		reg_loss_sigma += reg_losses.item()
 
         # 每10个iteration 打印一次训练信息，loss为10个iteration的平均
 		if i % 10 == 9 or i == len(train_loader)-1:
 			euc_loss_avg = euc_loss_sigma / (i-pre_i)
 			reg_loss_avg = reg_loss_sigma / (i-pre_i)
-			loss_avg = loss_sigma / (i-pre_i)
 			pre_i = i
 			euc_loss_sigma = 0.0
 			reg_loss_sigma = 0.0
-			loss_sigma = 0.0
-			print("Training: Epoch[{:0>3}/{:0>3}] Iteration[{:0>3}/{:0>3}] euc_losses: {:.8f} reg_losses: {:.8f} loss: {:.8f}".format(
-				epoch + 1, max_epoch, i + 1, len(train_loader), euc_loss_avg, reg_loss_avg, loss_avg))
+			print("Training: Epoch[{:0>3}/{:0>3}] Iteration[{:0>3}/{:0>3}] euc_losses: {:.8f} reg_losses: {:.8f}".format(
+				epoch + 1, max_epoch, i + 1, len(train_loader), euc_loss_avg, reg_loss_avg))
 		#scheduler.step()
 
 	euc_loss_sigma = 0.0
 	reg_loss_sigma = 0.0
-	loss_sigma = 0.0
 	net.eval()
 	for i, data in enumerate(test_loader):
 		images, coord_labels, heatmaps_labels, name = data
@@ -237,19 +229,17 @@ for epoch in range(max_epoch):
 			cv2.imwrite('./test_predict/'+name[b]+'-joints.png', joint_image)
 
 		# 计算loss
-		euc_losses = dsntnn.euclidean_losses(coords, coord_labels)
-		reg_losses = dsntnn.js_reg_losses(heatmaps, coord_labels, sigma_t=1.0)
-		loss = dsntnn.average_loss(euc_losses + reg_losses)
+		euc_losses = criterion(coords, coord_labels.float())
+		reg_losses = criterion(heatmaps, heatmaps_labels.float())
+		loss = euc_losses + reg_losses
 
-		euc_loss_sigma += dsntnn.average_loss(euc_losses).item()
-		reg_loss_sigma += dsntnn.average_loss(reg_losses).item()
-		loss_sigma += loss.item()
+		euc_loss_sigma += euc_losses.item()
+		reg_loss_sigma += reg_losses.item()
 
 	euc_loss_avg = euc_loss_sigma / len(test_loader)
 	reg_loss_avg = reg_loss_sigma / len(test_loader)
-	loss_avg = loss_sigma / len(test_loader)
-	print("Testing: Epoch[{:0>3}/{:0>3}] euc_losses: {:.8f} reg_losses: {:.8f}  loss: {:.8f}".format(
-		epoch + 1, max_epoch, euc_loss_avg, reg_loss_avg, loss_avg))
+	print("Testing: Epoch[{:0>3}/{:0>3}] euc_losses: {:.8f} reg_losses: {:.8f}".format(
+		epoch + 1, max_epoch, euc_loss_avg, reg_loss_avg))
 
 PATH = 'joint_model_net.pth'
 torch.save(net.state_dict(), PATH)
